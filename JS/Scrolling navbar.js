9c8440e00a7024d105d9561a525e796a9cb7e702
Md. Seaum Ibne Mamun <94161634+SeaumIbneMamun@users.noreply.github.com>
2023-10-03 12:43:37 +0600

    Add files via upload

--- JS/Scrolling navbar.js
+++ JS/Scrolling navbar.js
@@ -1,18 +1,39 @@
-const nav = document.querySelector('#navigation');
-const navTop = nav.offsetTop;
+// function changeBg(){
+//     var navbar = document.getElementById('navigation');
+//     var scrollValue = window.scrollY;
+//     // console.log(scrollValue);
+//     if(scrollValue < 150){
+//         navbar.classList.remove('bgColor');
+//     } else {
+//         navbar.classList.add('bgColor');
+//     }
+// }
 
-function stickyNavigation() {
-    console.log('navTop = ' + navTop);
-    console.log('scrollY = ' + window.scrollY);
-}
-
-window.addEventListener('scroll', stickyNavigation);
+// window.addEventListener('scroll', changeBg);
 
 
-function stickyNavigation() {
-    if (window.scrollY >= navTop) {
-        document.body.classList.add('fixed-nav');
-    } else {
-        document.body.classList.remove('fixed-nav');
-    }
-}
+
+// const nav = document.querySelector('#navigation');
+// const navTop = nav.offsetTop;
+
+// function stickyNavigation() {
+//     console.log('navTop = ' + navTop);
+//     console.log('scrollY = ' + window.scrollY);
+// }
+
+// window.addEventListener('scroll', stickyNavigation);
+
+
+// function stickyNavigation() {
+//     if (window.scrollY >= navTop) {
+//         document.body.classList.add('fixed-nav');
+//     } else {
+//         document.body.classList.remove('fixed-nav');
+//     }
+// }
+
+
+window.addEventListener("scroll", function(){
+    var header = this.document.querySelector(".flex");
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+})
